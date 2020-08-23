c033c4336990a631f693a4c9e3502e69a058bf19
unknown <akangah89@gmail.com>
2020-08-24 00:34:58 +0100

    Base project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import {InputGroup, FormControl, Button} from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+    render(){
+        return(
+            <>
+                <Navbar />
+                <p style={{
+                    position: 'absolute', left: '50%', top: '50%',
+                    transform: 'translate(-50%, -50%)'
+                }}>
+                Ayyy
+                </p>
+            </>
+
+        )
+    }
 }
-
-export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Navbar(){
+    return(
+        <p>
+            This is the navbar
+        </p>
+    )
+}
