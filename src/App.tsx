6848494140a4691a8b42c8ee46ad1a86e12b6bb0
unknown <akangah89@gmail.com>
2020-08-24 02:30:19 +0100

    Added centered component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import {InputGroup, FormControl, Button} from 'react-bootstrap';
+import DownloadText from './components/TextField';
 
 export default class App extends React.Component{
     render(){
         return(
             <>
                 <Navbar />
-                <p style={{
+                <div style={{
                     position: 'absolute', left: '50%', top: '50%',
                     transform: 'translate(-50%, -50%)'
                 }}>
-                Ayyy
-                </p>
+                    <DownloadText />
+                </div>
             </>
 
         )
